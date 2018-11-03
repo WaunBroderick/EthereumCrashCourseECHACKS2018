e1b8c542a0fdc9248d81b478fac5b7588aea3ac0
Waun Broderick <waunbroderick@gmail.com>
2018-11-03 08:56:56 -0400

    commented some extra lines in simpleCounter example

--- Remix Contracts/SimpleCounter.sol
+++ Remix Contracts/SimpleCounter.sol
@@ -2,11 +2,14 @@ pragma solidity 0.4.24;
 
 contract SimpleCounter {
     int counter; // state variable
-    address owner;
+    address owner; 
 
     constructor() public {
         counter = 0;
-        owner = msg.sender;
+        owner = msg.sender; //Global Variable to extract address of the message sender
+        //msg.value is another global variable to extract the amount of ether being sent
+        //ex. require(msg.value > 0.1 ether);
+        
     }
 
     function getCounter() view public returns(int) {
